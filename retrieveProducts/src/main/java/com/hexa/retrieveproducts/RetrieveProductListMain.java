76747b4063f98625b2a4f67dad419d46546abf6f
GSDTeam <GSDTeam@EBHVR15-03-01.hexa.training.local>
2018-10-19 17:51:36 +0530

    Checkstyle issues resolved

--- retrieveProducts/src/main/java/com/hexa/retrieveproducts/RetrieveProductListMain.java
+++ retrieveProducts/src/main/java/com/hexa/retrieveproducts/RetrieveProductListMain.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RetrieveProductListMain {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RetrieveProductListMain.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(RetrieveProductListMain.class, args);
+  }
 }
--- retrieveProducts/src/main/java/com/hexa/retrieveproducts/controller/ProductListController.java
+++ retrieveProducts/src/main/java/com/hexa/retrieveproducts/controller/ProductListController.java
@@ -1,5 +1,11 @@
 package com.hexa.retrieveproducts.controller;
 
+import com.hexa.retrieveproducts.model.Products;
+import com.hexa.retrieveproducts.repository.ProductsRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import java.util.List;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,42 +14,39 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.hexa.retrieveproducts.repository.ProductsRepository;
-import com.hexa.retrieveproducts.model.Products;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
-
-@CrossOrigin(origins= {"*"})
+@CrossOrigin(origins = {"*"})
 @RestController
 @RequestMapping("/products")
-@Api(value="FlipKart", description="To List all the Products available in Flipkart.") 
+@Api(value = "FlipKart",description = "To List all the Products available in Flipkart.") 
 public class ProductListController {
-	
-	@Autowired
-	private ProductsRepository productsRepository;
+  
+  @Autowired
+  private ProductsRepository productsRepository;
 
-	@ApiOperation(value = "View a list of available products", response = Iterable.class)
-	@ApiResponses(value = {
-	        @ApiResponse(code = 200, message = "Successfully retrieved list"),
-	        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
-	        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
-	        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
-	}
-	)	
-	@RequestMapping(value="/",method=RequestMethod.GET)
-	public List<Products> getAllProducts() {
-		
-		return productsRepository.findAll();
-	}
-	
-	@ApiOperation(value = "To Get the details of a product")
-	@RequestMapping(value="/{productId}", method=RequestMethod.GET)
-	public Products getProductDetails(@PathVariable("productId") ObjectId id) {
-		
-		String productID = id.toString();
-		return productsRepository.findBy_id(productID);
-	}
+  @ApiOperation(value = "View a list of available products", response = Iterable.class)
+  @ApiResponses(value = {
+          @ApiResponse(code = 200, message = "Successfully retrieved list"),
+          @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+          @ApiResponse(code = 403, message = "Forbidden resource"),
+          @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+      }
+  )  
+  @RequestMapping(value = "/",method = RequestMethod.GET)
+  public List<Products> getAllProducts() {
+    
+    return productsRepository.findAll();
+  }
+  /**
+   * This method is to get the product details.
+   * @param id
+   * @return
+   */  
+  @ApiOperation(value = "To Get the details of a product")  
+  @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+  public Products getProductDetails(@PathVariable("productId") ObjectId id) {
+    
+    String productId = id.toString();
+    return productsRepository.findBy_id(productId);
+  }
 }
